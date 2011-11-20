3f408b86bffeb36198b8f92fddfacc36530218de
Aaron Alexander <nerdfiles@gmail.com>
2011-11-20 16:01:18 -0600

    workin lastfm

--- _themes/nerdfiles_net_dev/_assets/workers/lastfm.js
+++ _themes/nerdfiles_net_dev/_assets/workers/lastfm.js
@@ -1,37 +1,36 @@
-  var httpRequest;
-
-  function makeRequest(url) {
-    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-      httpRequest = new XMLHttpRequest();
-    } else if (window.ActiveXObject) { // IE
-      try {
-        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-      } 
-      catch (e) {
-        try {
-          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-        } 
-        catch (e) {}
-      }
-    }
-
-    if (!httpRequest) {
-      alert('Giving up :( Cannot create an XMLHTTP instance');
-      return false;
-    }
-    httpRequest.onreadystatechange = alertContents;
-    httpRequest.open('GET', url);
-    httpRequest.send();
+var httpRequest;
+function makeRequest(url) {
+if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+  httpRequest = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE
+  try {
+    httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+  } 
+  catch (e) {
+    try {
+      httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    } 
+    catch (e) {}
   }
+}
 
-  function alertContents() {
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        alert(httpRequest.responseText);
-    } else {
-      alert('There was a problem with the request.');
-    }
-  }
+if (!httpRequest) {
+  alert('Giving up :( Cannot create an XMLHTTP instance');
+  return false;
+}
+httpRequest.onreadystatechange = alertContents;
+httpRequest.open('GET', url);
+httpRequest.send();
+}
+
+function alertContents() {
+if (httpRequest.readyState === 4) {
+  if (httpRequest.status === 200) {
+    alert(httpRequest.responseText);
+} else {
+  alert('There was a problem with the request.');
+}
+}
 
 function msgr(e) {
 
