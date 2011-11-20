7d2768e5d3dd915542a5015ac4b35e8ffb8d90ca
Aaron Alexander <nerdfiles@gmail.com>
2011-11-20 16:42:58 -0600

    first

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,14 @@
-set(fab_user='admin',
-     fab_hosts=['bristowgroup.com'],
-     root='/',
-     site='clientsite')
+set(fab_user='nerdfiles',
+     fab_hosts=['nerdfiles.net'],
+     root='/home/nerdfiles/webapps/django/nerdfiles_net/',
+     site='nerdfiles.net')
 
 def deploy():
     local('git archive --format=tar HEAD | gzip > $(site).tar.gz')
-    run('rm -rf $(root)$(site)')
+    #run('rm -rf $(root)$(site)')
     put('$(site).tar.gz', '$(root)$(site).tar.gz')
-    run('cd $(root)$(site) && tar zxf $(site).tar.gz')
-    restart()
+    #run('cd $(root)$(site) && tar zxf $(site).tar.gz')
+    #restart()
 
 def restart():
     run('sh $(root)$(site)/restart.sh')
