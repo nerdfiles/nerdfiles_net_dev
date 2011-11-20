f32a995fbe6be0518324d321c5d8527bce6884ef
Aaron Alexander <nerdfiles@gmail.com>
2011-11-20 14:11:02 -0600

    jquery dev on lastfm

---
+++ _themes/nerdfiles_net_dev/_assets/js/plugins.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// the semi-colon before function invocation is a safety net against concatenated 
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+    var pluginName = 'lastfm',
+        defaults = {
+        };
+
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
+        this.options = $.extend( {}, defaults, options) ;
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype.init = function () {
+        var $elem = this.element,
+        	opts = this.options,
+        	data = $.getJSON('http://nerdfiles.net/lastfm/');
+
+       	console.log(data);
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+            }
+        });
+    }
+
+})( jQuery, window, document );
+
