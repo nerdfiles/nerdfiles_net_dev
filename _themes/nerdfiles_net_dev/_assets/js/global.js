7740f42bb7ac82fd779f8830b9c9e6c56b22361f
nerdfiles <nerdfiles@gmail.com>
2011-12-25 21:55:30 -0600

    cleanu

--- _themes/nerdfiles_net_dev/_assets/js/global.js
+++ _themes/nerdfiles_net_dev/_assets/js/global.js
@@ -12,6 +12,34 @@
 				.weekly_artists();
 	    */
 
+    // wierd scroll-y links
+    
+    $("a[href^='#']").bind('click', function(e) {
+      
+      e.preventDefault();
+      
+      var $self = $(this),
+          target = this.hash,
+          $target = $(target);
+      
+      $('html, body').animate({
+        'scrollTop': ($target.offset().top)
+      }, 750, 'swing', function() {
+      
+        if ("onhashchange" in window) {
+          
+          //$target.prop('id', '');
+          //window.location.hash = target;
+          //$target.prop('id', target.replace('#', ''));
+          
+        } else {
+          //window.location.hash = target;
+        }
+        
+      });
+          
+    });
+
     $('a.external').attr('target', '_blank');
 
 	});
