13c700a87e631d740dbe1ee1ce3e87f2a7a575bd
Aaron Alexander <nerdfiles@gmail.com>
2012-06-13 14:19:14 -0500

    cleanup

--- _themes/nerdfiles_net_dev/_static/js/global.js
+++ _themes/nerdfiles_net_dev/_static/js/global.js
@@ -1,12 +1,47 @@
 "use strict";
 
-;(function($) {
+;(function(window, document, $) {
 
 	$(function() {
+	
+        // lastfm hookup test
+        /*
 		$('.mod-lastfm .inner')
 			.djangoLastFM()
 			.data('djangoLastFM')
 				.weekly_artists();
+	    */
+
+    // wierd scroll-y links
+    
+    $("a[href^='#']").bind('click', function(e) {
+      
+      e.preventDefault();
+      
+      var $self = $(this),
+          target = this.hash,
+          $target = $(target);
+      
+      $('html, body').animate({
+        'scrollTop': ($target.offset().top)
+      }, 750, 'swing', function() {
+      
+        if ("onhashchange" in window) {
+          
+          //$target.prop('id', '');
+          //window.location.hash = target;
+          //$target.prop('id', target.replace('#', ''));
+          
+        } else {
+          //window.location.hash = target;
+        }
+        
+      });
+          
+    });
+
+    $('a.external').attr('target', '_blank');
+
 	});
 
-})(jQuery);
+})(window, document, jQuery);
--- _themes/nerdfiles_net_dev/_static/workers/lastfm.js
+++ _themes/nerdfiles_net_dev/_static/workers/lastfm.js
@@ -1,5 +1,7 @@
 var httpRequest;
+
 function makeRequest(url) {
+
 if (window.XMLHttpRequest) { // Mozilla, Safari, ...
   httpRequest = new XMLHttpRequest();
 } else if (window.ActiveXObject) { // IE
@@ -18,18 +20,22 @@ if (!httpRequest) {
   alert('Giving up :( Cannot create an XMLHTTP instance');
   return false;
 }
+
 httpRequest.onreadystatechange = alertContents;
 httpRequest.open('GET', url);
 httpRequest.send();
+
 }
 
 function alertContents() {
+
 if (httpRequest.readyState === 4) {
   if (httpRequest.status === 200) {
     alert(httpRequest.responseText);
 } else {
   alert('There was a problem with the request.');
 }
+
 }
 
 function msgr(e) {
