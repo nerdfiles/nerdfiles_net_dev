d08b859c3e5ca24886cbfca8335941ce4709e0b7
nerdfiles <nerdfiles@gmail.com>
2012-07-31 18:12:14 -0500

    cleanup local

--- settings_dev.py
+++ settings_dev.py
@@ -4,7 +4,7 @@ import os
 
 # == VENV ======================================== #
 
-VENV_ROOT = os.path.join('Users','nerdfiles','.virtualenvs','nerdfiles_net_dev')
+# VENV_ROOT = os.path.join('','nerdfiles','.virtualenvs','nerdfiles_net_dev')
 # VENV_ROOT = os.path.join('')
 
 
@@ -13,5 +13,27 @@ VENV_ROOT = os.path.join('Users','nerdfiles','.virtualenvs','nerdfiles_net_dev')
 LOCAL_DEVELOPMENT = True
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+
+# == DB ======================================= #
+
+"""
+  @engines 'postgresql_psycopg2'|'postgresql'|'mysql'|'sqlite3'|'oracle'
+  @note If using local settings, this will be overridden.
+"""
+
+DATABASE_ENGINE = 'django.db.backends.sqlite3'
+DATABASE_NAME = 'dev.db'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+'''
+DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
+DATABASE_NAME = 'nerdfiles_website'
+DATABASE_USER = 'nerdfiles'
+DATABASE_PASSWORD = 'cbc8416e'
+'''
 
 
