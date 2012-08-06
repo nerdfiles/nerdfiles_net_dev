a1fc23967c58a7dbf777e427224f9a8cebf14072
nerdfiles <nerdfiles@gmail.com>
2012-08-06 17:22:08 -0500

    cleanup

---
+++ nerdfiles_net_dev/settings_dev.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+# == VENV ======================================== #
+
+# VENV_ROOT = os.path.join('','nerdfiles','.virtualenvs','nerdfiles_net_dev')
+# VENV_ROOT = os.path.join('')
+
+
+# == DEBUG == #
+
+LOCAL_DEVELOPMENT = False
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+
+# == DB ======================================= #
+
+#DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
+#DATABASE_NAME = 'nerdfiles_website'
+#DATABASE_USER = 'nerdfiles'
+#DATABASE_PASSWORD = 'f0xf0x0!6'
+#DATABASE_HOST = 'localhost'
+#DATABASE_PORT = '5432'
+
+DATABASE_ENGINE = 'django.db.backends.sqlite3'
+DATABASE_NAME = 'nerdfiles_net_dev.db'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
