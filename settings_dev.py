71390668313c5972edb69a80866c972e79c6de23
nerdfiles <nerdfiles@gmail.com>
2012-08-06 13:26:22 -0500

    cleanup

--- settings_dev.py
+++ settings_dev.py
@@ -17,10 +17,17 @@ TEMPLATE_DEBUG = DEBUG
 
 # == DB ======================================= #
 
-DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
-DATABASE_NAME = 'nerdfiles_website'
-DATABASE_USER = 'nerdfiles'
-DATABASE_PASSWORD = 'f0xf0x0!6'
+#DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
+#DATABASE_NAME = 'nerdfiles_website'
+#DATABASE_USER = 'nerdfiles'
+#DATABASE_PASSWORD = 'f0xf0x0!6'
 #DATABASE_HOST = 'localhost'
-DATABASE_PORT = '5432'
+#DATABASE_PORT = '5432'
+
+DATABASE_ENGINE = 'django.db.backends.sqlite3'
+DATABASE_NAME = 'nerdfiles_net_dev.db'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
 
