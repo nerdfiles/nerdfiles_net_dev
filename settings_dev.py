36faea98f80e1b935cde3502a138b15b3e610fb9
nerdfiles <nerdfiles@gmail.com>
2012-08-06 11:43:13 -0500

    cleanup

--- settings_dev.py
+++ settings_dev.py
@@ -22,10 +22,10 @@ TEMPLATE_DEBUG = DEBUG
   @note If using local settings, this will be overridden.
 """
 
-DATABASE_ENGINE = 'django.db.backends.postgresql'
+DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
 DATABASE_NAME = 'nerdfiles_website'
 DATABASE_USER = 'nerdfiles'
 DATABASE_PASSWORD = 'cbc8416e'
-DATABASE_HOST = ''
-DATABASE_PORT = ''
+#DATABASE_HOST = ''
+#DATABASE_PORT = ''
 
