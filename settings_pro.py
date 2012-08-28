8ff145a0b05f591678b7c706ec01da0d0458e78b
nerdfiles <nerdfiles@gmail.com>
2012-08-27 21:25:13 -0500

    cleanup auth

--- settings_pro.py
+++ settings_pro.py
@@ -167,10 +167,16 @@ TEMPLATE_LOADERS = (
 # == TEMPLATE LOADERS ======================================= #
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-  'django.core.context_processors.auth',
-  #'django.contrib.auth.context_processors.auth',
-  'django.core.context_processors.i18n',
+ 
   'django.core.context_processors.request',
+  'django.contrib.auth.context_processors.auth',
+  'django.core.context_processors.auth',
+
+  #'django.core.context_processors.auth',
+  #'django.contrib.auth.context_processors.auth',
+ 
+  'django.core.context_processors.i18n',
+  #'django.core.context_processors.request',
   'django.core.context_processors.media',
   'django.core.context_processors.static',
 
