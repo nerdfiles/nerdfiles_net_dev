9c27fdf7ce541f0a456450b7a896aa316fc2a935
nerdfiles <nerdfiles@gmail.com>
2012-08-27 21:38:38 -0500

    cleanup

--- settings_pro.py
+++ settings_pro.py
@@ -168,9 +168,9 @@ TEMPLATE_LOADERS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
  
-  'django.core.context_processors.request',
   'django.contrib.auth.context_processors.auth',
-  'django.core.context_processors.auth',
+  'django.core.context_processors.request',
+  #'django.core.context_processors.auth',
 
   #'django.core.context_processors.auth',
   #'django.contrib.auth.context_processors.auth',
