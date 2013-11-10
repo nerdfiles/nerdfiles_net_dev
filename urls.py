74357b841d298d3e71b23e0e590434c8b7087bcc
nerdfiles <nerdfiles@gmail.com>
2013-11-10 00:04:33 +0000

    Remove underscore.

--- urls.py
+++ urls.py
@@ -48,10 +48,10 @@ urlpatterns = patterns('',
 
 urlpatterns = patterns('',
 
-  url(r'^_assets(?P<path>.*)$', 'django.views.static.serve',
+  url(r'^assets(?P<path>.*)$', 'django.views.static.serve',
 
   {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-  url(r'^_static(?P<path>.*)$', 'django.views.static.serve',
+  url(r'^static(?P<path>.*)$', 'django.views.static.serve',
 
   {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
   url(r'', include('django.contrib.staticfiles.urls')),
