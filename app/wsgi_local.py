9b020564067141c7874cc89354a8c2e167bef9e4
nerdfiles <nerdfiles@gmail.com>
2015-04-25 15:38:35 -0500

    Proper wsgi setup.

--- app/wsgi_local.py
+++ app/wsgi_local.py
@@ -3,21 +3,21 @@ import sys
 import site
 from os import urandom
 
-site.addsitedir('/home/nerdfiles/.virtualenvs/jangy177/lib/python2.7/site-packages')
+site.addsitedir('/Users/nerdfiles/.pyenv/versions/django_nerdfiles_net/lib/python2.7/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
-path = '/home/nerdfiles/webapps/jangy177/nerdfiles_net_dev/'
+path = '/Users/nerdfiles/Projects/nerdfiles_net/'
 if path not in sys.path:
   sys.path.append(path)
   os.chdir(path)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-activate_this = os.path.expanduser("/home/nerdfiles/.virtualenvs/jangy177/bin/activate_this.py")
+activate_this = os.path.expanduser("/Users/nerdfiles/.pyenv/versions/django_nerdfiles_net/bin/activate_this.py")
 execfile(activate_this, dict(__file__=activate_this))
 
-project = '/home/nerdfiles/webapps/jangy177/nerdfiles_net_dev/'
+project = '/Users/nerdfiles/Projects/nerdfiles_net/'
 sys.path.insert(0, project)
 workspace = os.path.dirname(project)
 sys.path.append(workspace)
