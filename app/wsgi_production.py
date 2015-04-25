5207e6d6247d495d011b356652463f91f610a3e5
nerdfiles <nerdfiles@gmail.com>
2015-04-25 15:26:52 -0500

    Prod wsgi.

--- app/wsgi_production.py
+++ app/wsgi_production.py
@@ -6,9 +6,7 @@ import sys
 import site
 from os import urandom
 
-#site.addsitedir('/home/nerdfiles/.virtualenvs/jangy177/lib/python2.7/site-packages')
-site.addsitedir('/Users/nerdfiles/.pyenv/versions/django_nerdfiles_net/lib/python2.7/site-packages')
-
+site.addsitedir('/home/nerdfiles/.virtualenvs/jangy177/lib/python2.7/site-packages')
 
 #from django.core.handlers.wsgi import WSGIHandler
 from django.core.wsgi import get_wsgi_application
@@ -16,8 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 
 
-#path = '/home/nerdfiles/webapps/jangy177/nerdfiles_net_dev/'
-path = '/Users/nerdfiles/Projects/nerdfiles_net/'
+path = '/home/nerdfiles/webapps/jangy177/nerdfiles_net_dev/'
 if path not in sys.path:
   sys.path.append(path)
   os.chdir(path)
@@ -28,10 +25,10 @@ if path not in sys.path:
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-activate_this = os.path.expanduser("/Users/nerdfiles/.pyenv/versions/django_nerdfiles_net/bin/activate_this.py")
+activate_this = os.path.expanduser("/home/nerdfiles/.virtualenvs/jangy177/bin/activate_this.py")
 execfile(activate_this, dict(__file__=activate_this))
 
-project = '/Users/nerdfiles/Projects/nerdfiles_net/'
+project = '/home/nerdfiles/webapps/jangy177/nerdfiles_net_dev/'
 sys.path.insert(0, project)
 workspace = os.path.dirname(project)
 sys.path.append(workspace)
@@ -48,4 +45,3 @@ from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 '''
-
